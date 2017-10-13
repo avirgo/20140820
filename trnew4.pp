a671cfa905922f118fe41f3466b72b5bba9443a5
avirgo <avirgo@users.noreply.github.com>
2017-10-13 16:34:49 -0700

    Add files via upload

---
+++ trnew4.pp
@@ -0,0 +1,123 @@
+$howees = [
+[ "cdvra00a0846.wellsfargo.com", "ctrdev-os1.wellsfargo.com", "10.65.22.52", {},],
+[ "cdvra00a0847.wellsfargo.com", "ctrdev-bs1.wellsfargo.com", "10.65.22.53", {},],
+[ "cdvra00a0848.wellsfargo.com", "ctrdev-wa1.wellsfargo.com", "10.65.22.54", {},],
+[ "cdvrw00a0103.wellsfargo.com", "ctrdev-wm1.wellsfargo.com", "10.65.22.48", {"_CTRWS_SVC" => "d_cdvrw00a0103_12022", "_CTUUI_SVC" => "d_cdvrw00a0103_12032", "_RCM_SVC" => "d_cdvrw00a0103_12012", "_CTUWS_SVC" => "d_cdvrw00a0103_12042", "_RCMBCH_SVC" => "d_cdvrw00a0103_12052"}],
+[ "cdvrw00a0104.wellsfargo.com", "ctrdev-wm2.wellsfargo.com", "10.65.22.49", {"_CTRWS_SVC" => "d_cdvrw00a0104_12022", "_CTUUI_SVC" => "d_cdvrw00a0104_12032", "_RCM_SVC" => "d_cdvrw00a0104_12012", "_CTUWS_SVC" => "d_cdvrw00a0104_12042", "_RCMBCH_SVC" => "d_cdvrw00a0104_12052"}],
+[ "cdvrw00a0105.wellsfargo.com", "ctrdev-ws1.wellsfargo.com", "10.65.22.50", {},],
+[ "cdvrw00a0106.wellsfargo.com", "ctrdev-ws2.wellsfargo.com", "10.65.22.51", {},],
+[ "cnvra00a0846.wellsfargo.com", "ctret-os1.wellsfargo.com", "10.65.22.59", {},],
+[ "cnvra00a0847.wellsfargo.com", "ctret-bs1.wellsfargo.com", "10.65.22.60", {},],
+[ "cnvra00a0848.wellsfargo.com", "ctret-wa1.wellsfargo.com", "10.65.22.61", {},],
+[ "cnvrw00a0103.wellsfargo.com", "ctret-wm1.wellsfargo.com", "10.65.22.55", {"_CTRWS_SVC" => "e_cnvrw00a0103_12022", "_CTUUI_SVC" => "e_cnvrw00a0103_12032", "_RCM_SVC" => "e_cnvrw00a0103_12012", "_CTUWS_SVC" => "e_cnvrw00a0103_12042", "_RCMBCH_SVC" => "e_cnvrw00a0103_12052"},],
+[ "cnvrw00a0104.wellsfargo.com", "ctret-wm2.wellsfargo.com", "10.65.22.56", {"_CTRWS_SVC" => "e_cnvrw00a0104_12022", "_CTUUI_SVC" => "e_cnvrw00a0104_12032", "_RCM_SVC" => "e_cnvrw00a0104_12012", "_CTUWS_SVC" => "e_cnvrw00a0104_12042", "_RCMBCH_SVC" => "e_cnvrw00a0104_12052"},],
+[ "cnvrw00a0105.wellsfargo.com", "ctret-ws1.wellsfargo.com", "10.65.22.57", {},],
+[ "cnvrw00a0106.wellsfargo.com", "ctret-ws2.wellsfargo.com", "10.65.22.58", {},],
+[ "cpvra00a0846.wellsfargo.com", "ctrprod-os1.wellsfargo.com", "162.101.2.79", {},],
+[ "cpvra00a0847.wellsfargo.com", "ctrprod-os2.wellsfargo.com", "162.101.2.80", {},],
+[ "cpvra00a0848.wellsfargo.com", "ctrprod-bs1.wellsfargo.com", "162.101.2.81", {},],
+[ "cpvra00a0849.wellsfargo.com", "ctrprod-wa1.wellsfargo.com", "162.101.2.82", {},],
+[ "cpvrw00a0103.wellsfargo.com", "ctrprod-wm1.wellsfargo.com", "162.101.2.70", {"_CTRWS_SVC" => "p_cpvrw00a0103_12022", "_CTUUI_SVC" => "p_cpvrw00a0103_12032", "_RCM_SVC" => "p_cpvrw00a0103_12012", "_CTUWS_SVC" => "p_cpvrw00a0103_12042", "_RCMBCH_SVC" => "p_cpvrw00a0103_12052"},],
+[ "cpvrw00a0104.wellsfargo.com", "ctrprod-wm2.wellsfargo.com", "162.101.2.71", {"_CTRWS_SVC" => "p_cpvrw00a0104_12022", "_CTUUI_SVC" => "p_cpvrw00a0104_12032", "_RCM_SVC" => "p_cpvrw00a0104_12012", "_CTUWS_SVC" => "p_cpvrw00a0104_12042", "_RCMBCH_SVC" => "p_cpvrw00a0104_12052"},],
+[ "cpvrw00a0105.wellsfargo.com", "ctrprod-wm3.wellsfargo.com", "162.101.2.72", {"_CTRWS_SVC" => "p_cpvrw00a0105_12022", "_CTUUI_SVC" => "p_cpvrw00a0105_12032", "_RCM_SVC" => "p_cpvrw00a0105_12012", "_CTUWS_SVC" => "p_cpvrw00a0105_12042", "_RCMBCH_SVC" => "p_cpvrw00a0105_12052"},],
+[ "cpvrw00a0106.wellsfargo.com", "ctrprod-wm4.wellsfargo.com", "162.101.2.73", {"_CTRWS_SVC" => "p_cpvrw00a0106_12022", "_CTUUI_SVC" => "p_cpvrw00a0106_12032", "_RCM_SVC" => "p_cpvrw00a0106_12012", "_CTUWS_SVC" => "p_cpvrw00a0106_12042", "_RCMBCH_SVC" => "p_cpvrw00a0106_12052"},],
+[ "cpvrw00a0107.wellsfargo.com", "ctrprod-wm5.wellsfargo.com", "162.101.2.74", {"_CTRWS_SVC" => "p_cpvrw00a0107_12022", "_CTUUI_SVC" => "p_cpvrw00a0107_12032", "_RCM_SVC" => "p_cpvrw00a0107_12012", "_CTUWS_SVC" => "p_cpvrw00a0107_12042", "_RCMBCH_SVC" => "p_cpvrw00a0107_12052"},],
+[ "cpvrw00a0108.wellsfargo.com", "ctrprod-wm6.wellsfargo.com", "162.101.2.75", {"_CTRWS_SVC" => "p_cpvrw00a0108_12022", "_CTUUI_SVC" => "p_cpvrw00a0108_12032", "_RCM_SVC" => "p_cpvrw00a0108_12012", "_CTUWS_SVC" => "p_cpvrw00a0108_12042", "_RCMBCH_SVC" => "p_cpvrw00a0108_12052"},],
+[ "cpvrw00a0109.wellsfargo.com", "ctrprod-ws1.wellsfargo.com", "162.101.2.76", {},],
+[ "cpvrw00a0110.wellsfargo.com", "ctrprod-ws2.wellsfargo.com", "162.101.2.77", {},],
+[ "cpvrw00a0111.wellsfargo.com", "ctrprod-ws3.wellsfargo.com", "162.101.2.78", {},],
+[ "cpvwa00a2199.wellsfargo.com", "ctrprod-dps1.wellsfargo.com", "162.101.2.83", {},],
+[ "crvra00a0846.wellsfargo.com", "ctrbcp-os1.wellsfargo.com", "162.102.18.48", {},],
+[ "crvra00a0847.wellsfargo.com", "ctrbcp-os2.wellsfargo.com", "162.102.18.49", {},],
+[ "crvra00a0848.wellsfargo.com", "ctrbcp-bs1.wellsfargo.com", "162.102.18.50", {},],
+[ "crvra00a0849.wellsfargo.com", "ctrbcp-wa1.wellsfargo.com", "162.102.18.51", {},],
+[ "crvrw00a0103.wellsfargo.com", "ctrbcp-wm1.wellsfargo.com", "162.102.18.39", {"_CTRWS_SVC" => "b_crvrw00a0103_12022", "_CTUUI_SVC" => "b_crvrw00a0103_12032", "_RCM_SVC" => "b_crvrw00a0103_12012", "_CTUWS_SVC" => "b_crvrw00a0103_12042", "_RCMBCH_SVC" => "b_crvrw00a0103_12052"},],
+[ "crvrw00a0104.wellsfargo.com", "ctrbcp-wm2.wellsfargo.com", "162.102.18.40", {"_CTRWS_SVC" => "b_crvrw00a0104_12022", "_CTUUI_SVC" => "b_crvrw00a0104_12032", "_RCM_SVC" => "b_crvrw00a0104_12012", "_CTUWS_SVC" => "b_crvrw00a0104_12042", "_RCMBCH_SVC" => "b_crvrw00a0104_12052"},],
+[ "crvrw00a0105.wellsfargo.com", "ctrbcp-wm3.wellsfargo.com", "162.102.18.41", {"_CTRWS_SVC" => "b_crvrw00a0105_12022", "_CTUUI_SVC" => "b_crvrw00a0105_12032", "_RCM_SVC" => "b_crvrw00a0105_12012", "_CTUWS_SVC" => "b_crvrw00a0105_12042", "_RCMBCH_SVC" => "b_crvrw00a0105_12052"},],
+[ "crvrw00a0106.wellsfargo.com", "ctrbcp-wm4.wellsfargo.com", "162.102.18.42", {"_CTRWS_SVC" => "b_crvrw00a0106_12022", "_CTUUI_SVC" => "b_crvrw00a0106_12032", "_RCM_SVC" => "b_crvrw00a0106_12012", "_CTUWS_SVC" => "b_crvrw00a0106_12042", "_RCMBCH_SVC" => "b_crvrw00a0106_12052"},],
+[ "crvrw00a0107.wellsfargo.com", "ctrbcp-wm5.wellsfargo.com", "162.102.18.43", {"_CTRWS_SVC" => "b_crvrw00a0107_12022", "_CTUUI_SVC" => "b_crvrw00a0107_12032", "_RCM_SVC" => "b_crvrw00a0107_12012", "_CTUWS_SVC" => "b_crvrw00a0107_12042", "_RCMBCH_SVC" => "b_crvrw00a0107_12052"},],
+[ "crvrw00a0108.wellsfargo.com", "ctrbcp-wm6.wellsfargo.com", "162.102.18.44", {"_CTRWS_SVC" => "b_crvrw00a0108_12022", "_CTUUI_SVC" => "b_crvrw00a0108_12032", "_RCM_SVC" => "b_crvrw00a0108_12012", "_CTUWS_SVC" => "b_crvrw00a0108_12042", "_RCMBCH_SVC" => "b_crvrw00a0108_12052"},],
+[ "crvrw00a0109.wellsfargo.com", "ctrbcp-ws1.wellsfargo.com", "162.102.18.45", {},],
+[ "crvrw00a0110.wellsfargo.com", "ctrbcp-ws2.wellsfargo.com", "162.102.18.46", {},],
+[ "crvrw00a0111.wellsfargo.com", "ctrbcp-ws3.wellsfargo.com", "162.102.18.47", {},],
+[ "crvwa00a2199.wellsfargo.com", "ctrbcp-dps1.wellsfargo.com", "162.102.18.52", {},],
+[ "csvra00a0846.wellsfargo.com", "ctrist-os1.wellsfargo.com", "10.65.22.66", {},],
+[ "csvra00a0847.wellsfargo.com", "ctrist-bs1.wellsfargo.com", "10.65.22.67", {},],
+[ "csvra00a0848.wellsfargo.com", "ctrist-wa1.wellsfargo.com", "10.65.22.68", {},],
+[ "csvrw00a0103.wellsfargo.com", "ctrist-wm1.wellsfargo.com", "10.65.22.62", {"_CTRWS_SVC" => "i_csvrw00a0103_12022", "_CTUUI_SVC" => "i_csvrw00a0103_12032", "_RCM_SVC" => "i_csvrw00a0103_12012", "_CTUWS_SVC" => "i_csvrw00a0103_12042", "_RCMBCH_SVC" => "i_csvrw00a0103_12052"},],
+[ "csvrw00a0104.wellsfargo.com", "ctrist-wm2.wellsfargo.com", "10.65.22.63", {"_CTRWS_SVC" => "i_csvrw00a0104_12022", "_CTUUI_SVC" => "i_csvrw00a0104_12032", "_RCM_SVC" => "i_csvrw00a0104_12012", "_CTUWS_SVC" => "i_csvrw00a0104_12042", "_RCMBCH_SVC" => "i_csvrw00a0104_12052"},],
+[ "csvrw00a0105.wellsfargo.com", "ctrist-ws1.wellsfargo.com", "10.65.22.64", {},],
+[ "csvrw00a0106.wellsfargo.com", "ctrist-ws2.wellsfargo.com", "10.65.22.65", {},],
+[ "cuvra00a0846.wellsfargo.com", "ctrqa-os1.wellsfargo.com", "10.65.22.73", {},],
+[ "cuvra00a0847.wellsfargo.com", "ctrqa-bs1.wellsfargo.com", "10.65.22.74", {},],
+[ "cuvra00a0848.wellsfargo.com", "ctrqa-wa1.wellsfargo.com", "10.65.22.75", {},],
+[ "cuvrw00a0103.wellsfargo.com", "ctrqa-wm1.wellsfargo.com", "10.65.22.69", {"_CTRWS_SVC" => "q_cuvrw00a0103_12022", "_CTUUI_SVC" => "q_cuvrw00a0103_12032", "_RCM_SVC" => "q_cuvrw00a0103_12012", "_CTUWS_SVC" => "q_cuvrw00a0103_12042", "_RCMBCH_SVC" => "q_cuvrw00a0103_12052"},],
+[ "cuvrw00a0104.wellsfargo.com", "ctrqa-wm2.wellsfargo.com", "10.65.22.70", {"_CTRWS_SVC" => "q_cuvrw00a0104_12022", "_CTUUI_SVC" => "q_cuvrw00a0104_12032", "_RCM_SVC" => "q_cuvrw00a0104_12012", "_CTUWS_SVC" => "q_cuvrw00a0104_12042", "_RCMBCH_SVC" => "q_cuvrw00a0104_12052"},],
+[ "cuvrw00a0105.wellsfargo.com", "ctrqa-ws1.wellsfargo.com", "10.65.22.71", {},],
+[ "cuvrw00a0106.wellsfargo.com", "ctrqa-ws2.wellsfargo.com", "10.65.22.72", {},],
+[ "cxvra00a0846.wellsfargo.com", "ctruat-os1.wellsfargo.com", "10.65.22.82", {},],
+[ "cxvra00a0847.wellsfargo.com", "ctruat-os2.wellsfargo.com", "10.65.22.83", {},],
+[ "cxvra00a0848.wellsfargo.com", "ctruat-bs1.wellsfargo.com", "10.65.22.84", {},],
+[ "cxvra00a0849.wellsfargo.com", "ctruat-wa1.wellsfargo.com", "10.65.22.85", {},],
+[ "cxvrw00a0103.wellsfargo.com", "ctruat-wm1.wellsfargo.com", "10.65.22.76", {"_CTRWS_SVC" => "u_cxvrw00a0103_12022", "_CTUUI_SVC" => "u_cxvrw00a0103_12032", "_RCM_SVC" => "u_cxvrw00a0103_12012", "_CTUWS_SVC" => "u_cxvrw00a0103_12042", "_RCMBCH_SVC" => "u_cxvrw00a0103_12052"},],
+[ "cxvrw00a0104.wellsfargo.com", "ctruat-wm2.wellsfargo.com", "10.65.22.77", {"_CTRWS_SVC" => "u_cxvrw00a0104_12022", "_CTUUI_SVC" => "u_cxvrw00a0104_12032", "_RCM_SVC" => "u_cxvrw00a0104_12012", "_CTUWS_SVC" => "u_cxvrw00a0104_12042", "_RCMBCH_SVC" => "u_cxvrw00a0104_12052"},],
+[ "cxvrw00a0105.wellsfargo.com", "ctruat-wm3.wellsfargo.com", "10.65.22.78", {"_CTRWS_SVC" => "u_cxvrw00a0105_12022", "_CTUUI_SVC" => "u_cxvrw00a0105_12032", "_RCM_SVC" => "u_cxvrw00a0105_12012", "_CTUWS_SVC" => "u_cxvrw00a0105_12042", "_RCMBCH_SVC" => "u_cxvrw00a0105_12052"},],
+[ "cxvrw00a0106.wellsfargo.com", "ctruat-wm4.wellsfargo.com", "10.65.22.79", {"_CTRWS_SVC" => "u_cxvrw00a0106_12022", "_CTUUI_SVC" => "u_cxvrw00a0106_12032", "_RCM_SVC" => "u_cxvrw00a0106_12012", "_CTUWS_SVC" => "u_cxvrw00a0106_12042", "_RCMBCH_SVC" => "u_cxvrw00a0106_12052"},],
+[ "cxvrw00a0107.wellsfargo.com", "ctruat-ws1.wellsfargo.com", "10.65.22.80", {},],
+[ "cxvrw00a0108.wellsfargo.com", "ctruat-ws2.wellsfargo.com", "10.65.22.81", {},],
+]
+if $osfamily == 'windows'{
+   $tmpdir = "c:/temp/tmp"
+ } else {
+   $tmpdir = "/var/tmp"
+ }
+$g = { 'd' => "dev", 's' => "sit", 'p' => "prod", 'r' => "bcp", 'u' => "qa", 'x' => "uat", 'n' => "et" }
+$howees.each | $howee | {
+    [$hostnamee_fqdn, $hostnamee, $hostnamee_ip, $miscvars ] = $howee
+    $to_use = $hostnamee_fqdn ? { /^...w/ => "xiwizard_windowsserver_host", default => "xiwizard_linuxserver_host" }
+    $env = $hostnamee_fqdn ? {/^.(.)/ =>  $1, default => 'd'}
+    $v = $g[$env]
+    $hgn = inline_template("TR <%= @v.upcase -%> host group")
+
+    nagios_host { $hostnamee:
+   target                    => "${tmpdir}/${hostnamee}.cfg",
+   use                       =>  $to_use,
+   alias                     => "$hostnamee_fqdn",
+   display_name              => "$hostnamee_fqdn",
+   address                   => "$hostnamee_ip",
+   check_interval            => "5",
+   retry_interval            => "1",
+   check_period              => "xi_timeperiod_24x7",
+   contact_groups            => "Netcool - NONPROD,Test Group",
+   notification_interval     => "60",
+   notification_period       => "xi_timeperiod_24x7",
+   first_notification_delay  => "0",
+   notifications_enabled     => "1",
+   max_check_attempts    => "5",
+   register              => "1",
+   hostgroups            => [ $hgn, "RedHat Servers" ],
+    }
+
+	$miscvars.each | $k | {
+            [$key, $val] = $k
+	    notify{"miscvars ${key}: ${val}":}
+            augeas {"add ${key} => ${val} to ${hostnamee}":
+            changes => [ "set host/${key} ${val}",
+            ],
+            incl => "${tmpdir}/${hostnamee}.cfg",
+            lens => "NagiosObjects.lns",
+            }
+	}
+}
+$host_fqdn = $howees.map |$items| {$items[0]}
+$g.each |$k, $v| {
+  $ref = "^.${k}"
+  notice("ref is $ref")
+  $group = $host_fqdn.filter |$val| { $val =~ $ref}
+  nagios_hostgroup{ "${v}":
+    target => "${tmpdir}/hg_${v}.cfg",
+    hostgroup_name => inline_template("TR <%= @v.upcase -%> host group"),
+    alias => inline_template("TR <%= @v.upcase -%> servers"),
+    members => [$group],
+  }
+}
